6fe992c5aebf64b03c6ed8d785ec1679b9b390ac
Chris Clark <clark@ripple.com>
2015-01-21 16:09:19 -0800

    Refactor to remove BigInt class

--- convertbase.js
+++ convertbase.js
@@ -1,126 +1,113 @@
 // Copyright (c) 2014 Chris Clark
 
-function BigInt(value, radix) {
-  this._radix = radix || 10;
+function normalize(digits) {
+  while(digits[digits.length - 1] === 0) {
+    digits.pop();         // normalize to have no leading zeros
+  }
+  return digits;
+}
+
+function parseValue(value, radix) {
+  var digits = [];
+  radix = radix || 10;
   if (value && value.constructor === Array) {
-    this._digits = value.reverse();
+    digits = value;
   } else if (!isNaN(value)) {
-    this._digits = [];
     for(var i = 0; value > 0; i++) {
-      this._digits.push(value % this._radix);
-      value = Math.floor(value / this._radix);
+      digits.push(value % radix);
+      value = Math.floor(value / radix);
     }
   } else {
     throw new Error('Unrecognized value type: ' + (typeof value));
   }
-  while(this._digits[this._digits.length - 1] === 0)
-    this._digits.pop();         // normalize to have no leading zeros
+  return normalize(digits);
 }
 
-BigInt.prototype.getRadix = function() {
-  return this._radix;
-};
-
-BigInt.prototype.getDigits = function() {
-  return this._digits.slice(0).reverse();
-};
-
-BigInt.prototype.numberOfDigits = function() {
-  return this._digits.length;
-};
-
-BigInt.prototype.getDigit = function(i) {
-  return i < this.numberOfDigits() ? this._digits[i] : 0;
-};
-
-BigInt.prototype.add = function(n) {
+function add(leftDigits, rightDigits, radix) {
   var result = [];
   var carry = 0;
-  if (n.getRadix() !== this._radix) {
-    n = n.toBase(this._radix);
-  }
-  var size = Math.max(this.numberOfDigits(), n.numberOfDigits()) + 1;
+  var size = Math.max(leftDigits.length, rightDigits.length) + 1;
   for(var i = 0; i < size; i++) {
-    var sum = this.getDigit(i) + n.getDigit(i) + carry;
-    result.push(sum % this._radix);
-    carry = sum >= this._radix ? 1 : 0;
+    var sum = (leftDigits[i] || 0) + (rightDigits[i] || 0) + carry;
+    result.push(sum % radix);
+    carry = sum >= radix ? 1 : 0;
   }
-  return new BigInt(result.reverse(), this._radix);
-};
+  return normalize(result);
+}
 
-BigInt.prototype.shift = function(shift) {
-  if (shift === 0) {
-    return this;
+function shift(digits, size) {
+  var result = [];
+  for(var i = 0; i < size; i++) {
+    result.push(0);
+  }
+  return normalize(result.concat(digits));
+}
+
+function multiplyDigit(digits, digit, radix) {
+  var carry = 0;
+  var result = [];
+  for(var i = 0; i < digits.length; i++) {
+    var x = (digits[i] * digit) + carry;
+    result.push(x % radix);
+    carry = Math.floor(x / radix);
+  }
+  result.push(carry);
+  return normalize(result);
+}
+
+function multiply(leftDigits, rightDigits, radix) {
+  var result = [];
+  for(var i = 0; i < rightDigits.length; i++) {
+    var x = multiplyDigit(leftDigits, rightDigits[i], radix);
+    result = add(result, shift(x, i), radix);
+  }
+  return normalize(result);
+}
+
+function toBase(digits, oldRadix, newRadix) {
+  if (oldRadix === newRadix) {
+    return digits;
   }
   var result = [];
-  for(var i = 0; i < shift; i++)
-    result.push(0);
-  return new BigInt(result.concat(this._digits).reverse(), this._radix);
-};
+  var factor = parseValue(oldRadix, newRadix);
+  for(var i = 0; i < digits.length; i++) {
+    result = multiply(result, factor, newRadix);
+    var digit = digits[digits.length - i - 1];
+    result = add(result, parseValue(digit, newRadix), newRadix);
+  }
+  return normalize(result);
+}
 
-BigInt.prototype.multiply = function(n) {
-  function multiplyDigit(bigint, digit) {
-    var carry = 0;
-    var radix = bigint.getRadix();
-    var result = [];
-    for(var i = 0; i < bigint.numberOfDigits(); i++) {
-      var x = (bigint.getDigit(i) * digit) + carry;
-      result.push(x % radix);
-      carry = Math.floor(x / radix);
-    }
-    result.push(carry);
-    return new BigInt(result.reverse(), radix);
-  }
-  if (n.getRadix() !== this._radix) {
-    n = n.toBase(this._radix);
-  }
-  var result = new BigInt(0, this._radix);
-  for(var i = 0; i < n.numberOfDigits(); i++) {
-    result = result.add(multiplyDigit(this, n.getDigit(i)).shift(i));
-  }
-  return result;
-};
-
-BigInt.prototype.toBase = function(newRadix) {
-  var result = new BigInt(0, newRadix);
-  var factor = new BigInt(this._radix, newRadix);
-  for(var i = 0; i < this.numberOfDigits(); i++) {
-    result = result.multiply(factor);
-    var digit = this._digits[this.numberOfDigits() - i - 1];
-    result = result.add(new BigInt(digit, newRadix));
-  }
-  return result;
-};
-
-function toBigInt(value, radix) {
+function toDigits(value, radix) {
   if (radix === parseInt(radix) && radix >= 2) {
-    return new BigInt(value, radix);
+    return parseValue(value.reverse(), radix).reverse();
   } else if (typeof radix === 'string') {
     var alphabet = radix;
     var digits = value.split('').map(function(c) {
       return alphabet.indexOf(c);
     });
-    return new BigInt(digits, alphabet.length);
+    return parseValue(digits.reverse(), alphabet.length).reverse();
   } else {
     throw new Error('Invalid type for "radix"');
   }
 }
 
-function fromBigInt(bigint, radix) {
-  if (radix === parseInt(radix) && radix >= 2) {
-    return bigint.toBase(radix).getDigits();
-  } else if (typeof radix === 'string') {
-    var alphabet = radix;
-    return bigint.toBase(alphabet.length).getDigits().map(function(i) {
+function fromDigits(digits, oldRadix, newRadix) {
+  if (newRadix === parseInt(newRadix) && newRadix >= 2) {
+    return toBase(digits.reverse(), oldRadix, newRadix).reverse();
+  } else if (typeof newRadix === 'string') {
+    var alphabet = newRadix;
+    return toBase(digits.reverse(), oldRadix, alphabet.length).map(function(i) {
       return alphabet.charAt(i);
-    }).join('');
+    }).reverse().join('');
   } else {
     throw new Error('Invalid type for "radix"');
   }
 }
 
 function convertBase(value, fromBase, toBase) {
-    return fromBigInt(toBigInt(value, fromBase), toBase);
+    var fromRadix = typeof fromBase === 'string' ? fromBase.length : fromBase;
+    return fromDigits(toDigits(value, fromBase), fromRadix, toBase);
 }
 
 module.exports = convertBase;
